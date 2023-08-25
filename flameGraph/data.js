65874e6fff8449e38dc6039a2158efb94a05c4a3
beothorn <lucass@gmail.com>
2023-08-26 00:48:32 +0200

    Add flame graph

---
+++ flameGraph/data.js
@@ -0,0 +1,24 @@
+var data = {
+    "children": [
+        {
+            "children": [
+                {
+                    "name": "fastFunction",
+                    "value": 0
+                },
+                {
+                    "name": "slowFunction",
+                    "value": 1062
+                }
+            ],
+            "name": "functionThatCallsFunctions",
+            "value": 1062
+        },
+        {
+            "name": "getValue",
+            "value": 41
+        }
+    ],
+    "name": "main",
+    "value": 1110
+}
