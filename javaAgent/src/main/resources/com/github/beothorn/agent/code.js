365ab24a862a731b0441786bfd0954747e48c40f
beothorn <lucass@gmail.com>
2023-10-07 22:19:05 +0200

    starting merge on js

---
+++ javaAgent/src/main/resources/com/github/beothorn/agent/code.js
@@ -0,0 +1,9 @@
+// get al thread names
+// for each thread join
+// filter function
+
+let threadNames = Array.from(new Set(data.flatMap(e => e.map(ee => ee.thread))));
+
+
+
+data.flatMap(e => e.filter(ee => ee.thread == "main"))
