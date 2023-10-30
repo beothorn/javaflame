9bc0debe0b294d4951170ac75a39f36fc99d0866
beothorn <lucass@gmail.com>
2023-10-30 22:19:22 +0100

    Fix toString recursiveness, arg null pointer and toString exception

--- javaExampleApp/src/main/java/com/github/beothorn/AnotherClass.java
+++ javaExampleApp/src/main/java/com/github/beothorn/AnotherClass.java
@@ -5,7 +5,6 @@ public class AnotherClass {
     private final String value;
 
     public AnotherClass(String value){
-
         this.value = value;
     }
 
@@ -16,9 +15,18 @@ public class AnotherClass {
 
     private int getJ() {
         int j = 0;
-        for (int i = 0; i < 100000000; i++) {
+        for (int i = 0; i < 1000; i++) {
             j++;
         }
         return j;
+    }
+
+    @Override
+    public String toString() {
+        return recursive(this) + "xxxxx";
+    }
+
+    private static String recursive(AnotherClass c) {
+        return c.getValue();
     }
 }
