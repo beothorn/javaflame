7ac4ff878856ab32f6412abc6838f256e4fd9bc7
beothorn <lucass@gmail.com>
2023-11-01 12:41:20 +0100

    Example with sort algorithms comparision

---
+++ docs/code.js
@@ -0,0 +1,94 @@
+// get all thread names
+// for each thread join
+// filter function
+// join last child, first child if same start and name
+
+function mergeSnapshots(data) {
+    let threadNames = new Set(
+        data.map(d => 
+            Array.from(new Set(d.flatMap(s => s.thread)))
+        ).flatMap(s => s)
+    );
+
+    let result = [];
+    
+    for(let threadName of threadNames){
+        const snapshotsFromThread = data.map(d => d.filter(s => s.thread === threadName)).flatMap(s => s);
+        const firstSnapshot = snapshotsFromThread[0];
+        const lastSnapshot = snapshotsFromThread[snapshotsFromThread.length - 1];
+        if(firstSnapshot.span.name !== lastSnapshot.span.name){
+            throw new Error(`Root function should not change inside thread. 
+            Function names are different '${firstSnapshot.snapshotTime}' and '${lastSnapshot.snapshotTime}'`);
+        }
+        // sum of all values from root function using reduce
+        const mergedSnapshotForThread = snapshotsFromThread.reduce((snapA, snapB) => ({
+            "thread": threadName,
+            "snapshotTime": snapB.snapshotTime,
+            "span": mergeSpans(snapA.span, snapB.span)
+        }));
+        result.push(mergedSnapshotForThread);
+    }
+    
+    return result;
+}
+
+function mergeSpans(spanA, spanB) {
+    if(spanA.name!== spanB.name){
+        throw new Error(`Different spans can't be merged. 
+            Function names are different '${spanA.name}' and '${spanB.span.name}'`);
+    }
+    let finalSpan = {
+        "name": spanA.name,
+        "entryTime":spanA.entryTime,
+        "exitTime":spanB.exitTime,
+        "value":spanA.value + spanB.value
+    };
+    
+    if (spanA.children || spanB.children) {
+        const childrenA = spanA.children || [];
+        const childrenB = spanB.children || [];
+        finalSpan.children = mergeChildren(childrenA, childrenB);
+        if(spanB.exitTime == -1){
+            finalSpan.value = finalSpan.children.map(c => c.value).reduce((a, b) => a + b);
+            finalSpan.exitTime = spanA.entryTime + finalSpan.value;
+        }
+    }
+
+    return finalSpan;
+}
+
+function mergeChildren(childrenA, childrenB){
+    if(childrenB.length == 0) return childrenA;
+    // if last children from childrenA name and entry time is the same as first children from childrenB
+    // merge spans
+    const lastSpanOnChildrenA = childrenA[childrenA.length - 1];
+    const firstSpanOnChildrenB = childrenB[0];
+    if(lastSpanOnChildrenA.name === firstSpanOnChildrenB.name 
+        && lastSpanOnChildrenA.entryTime === firstSpanOnChildrenB.entryTime
+    ){
+        const intersection = mergeSpans(lastSpanOnChildrenA, firstSpanOnChildrenB);
+        const childrenAExceptLast = childrenA.slice(0, childrenA.length - 1);
+        const childrenBExceptFirst = childrenB.slice(1);
+        return childrenAExceptLast.concat(intersection).concat(childrenBExceptFirst);
+    }
+
+    return childrenA.concat(childrenB);
+}
+
+function increaseZeroValues(data) {
+    return data.map( d => ({
+      ...d,
+      "span": increaseSpanValue(d.span)
+    }));
+}
+
+function increaseSpanValue(span) {
+    let newSpan = {...span};
+    if(span.children && span.children.length > 0){
+        newSpan.children = span.children.map(increaseSpanValue);
+        newSpan.value = newSpan.children.map(c => c.value).reduce((a, b) => a + b);
+    } else {
+        if(newSpan.value == 0) newSpan.value = 1;
+    }
+    return newSpan;
+}
--- javaExampleApp/src/main/java/com/github/beothorn/sorts/BubbleSort.java
+++ javaExampleApp/src/main/java/com/github/beothorn/sorts/BubbleSort.java
@@ -2,51 +2,34 @@ package com.github.beothorn.sorts;
 
 public class BubbleSort {
 
-    static void bubbleSort(int arr[], int n)
-    {
-        int i, j, temp;
-        boolean swapped;
-        for (i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-
-                    // Swap arr[j] and arr[j+1]
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-
-            // If no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
-                break;
-        }
+    private static boolean isABiggerThanB(int a, int b){
+        return a > b;
     }
 
-    private static boolean swapIfBiggerThanNext(int[] array, int i) {
-        if (array[i] <= array[i + 1]) return false;
-        int temp = array[i];
-        array[i] = array[i+1];
-        array[i+1] = temp;
+    private static boolean swapIfBiggerThanNext(int cursorPosition, int[] array) {
+        if ( !isABiggerThanB(array[cursorPosition], array[cursorPosition+1]) ) return false;
+        int temp = array[cursorPosition];
+        array[cursorPosition] = array[cursorPosition+1];
+        array[cursorPosition+1] = temp;
         return true;
     }
 
     public static int[] sort(int[] array) {
-        boolean swapped;
-        int arrayLastIndex = array.length - 1;
-        // copy array to result
         int[] result = new int[array.length];
         System.arraycopy(array, 0, result, 0, array.length);
         for (int i = array.length - 1; i > 0; i--) {
-            swapped = false;
-            for (int cursor = 0; cursor < i; cursor++) {
-                swapped = swapIfBiggerThanNext(result, cursor);
-            }
-            if (!swapped) break;
+            boolean thereWasASwapping = bubbleUp(i, result);
+            boolean alreadyOrdered = !thereWasASwapping;
+            if (alreadyOrdered) break;
         }
         return result;
+    }
+
+    private static boolean bubbleUp(int i, int[] result) {
+        boolean swapped = false;
+        for (int cursor = 0; cursor < i; cursor++) {
+            swapped = swapIfBiggerThanNext(cursor, result) || swapped;
+        }
+        return swapped;
     }
 }
