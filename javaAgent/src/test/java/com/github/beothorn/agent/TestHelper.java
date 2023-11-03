5cbdd929857311a958f4f9102c2812040f2c4613
beothorn <lucass@gmail.com>
2023-11-03 22:06:04 +0100

    Easier tests with json

---
+++ javaAgent/src/test/java/com/github/beothorn/agent/TestHelper.java
@@ -0,0 +1,45 @@
+package com.github.beothorn.agent;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class TestHelper {
+
+    public static JSONObject thread(
+        String thread,
+        int snapshotTime,
+        JSONObject span
+    ){
+        try {
+            return new JSONObject()
+                .put("thread", thread)
+                .put("snapshotTime", snapshotTime)
+                .put("span", span);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static JSONObject span(
+        String name,
+        int entryTime,
+        int exitTime,
+        int value,
+        JSONObject... children
+    ){
+        try {
+            JSONObject result = new JSONObject()
+                .put("name", name)
+                .put("entryTime", entryTime)
+                .put("exitTime", exitTime)
+                .put("value", value);
+            if (children.length > 0) {
+                result.put("children", new JSONArray(children));
+            }
+            return result;
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
