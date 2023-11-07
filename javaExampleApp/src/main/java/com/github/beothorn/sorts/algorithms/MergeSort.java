e1ea07de0439154de11f901c054f2be073cb6e63
beothorn <lucass@gmail.com>
2023-11-07 23:09:17 +0100

    Add insertion sort and sort descriptions

--- javaExampleApp/src/main/java/com/github/beothorn/sorts/algorithms/MergeSort.java
+++ javaExampleApp/src/main/java/com/github/beothorn/sorts/algorithms/MergeSort.java
@@ -2,6 +2,25 @@ package com.github.beothorn.sorts.algorithms;
 
 import com.github.beothorn.sorts.Common;
 
+/**
+ * This MergeSort implementation may call more functions than actually needed.
+ * This is because I want the flamegraph to show exactly what is going on.
+ * MergeSort will apply a logic for merging two sorted list in a bigger list.
+ * It will do it in a recursive call, similar to a depth-first search.
+ * It will drill down the array recusively until it reaches the case where the sublist
+ * has only one or two elements. Then it will sort it and on the function
+ * that called this, it will merge the two sorted sublists.
+ * Each call will have bigger and bigger sublists, until it reaches the first call on
+ * the stack and two sorted lists are merged into one.
+ * When merging there are three cursors, the left, the right and the copy cursor.
+ * The left will traverse the left sublist, the right will traverse the right sublist.
+ * The copy cursor will traverse the resulting list.
+ * Each step will check if the smaller value is the one on the left cursor or on the right cursor.
+ * The smaller value is copied to the copy cursor and the cursor with the smaller value (left or right)
+ * is advanced.
+ * If any of the cursors reach the end of its sublist, then the other sublist is fully copied to the copy cursor.
+ * We use Common.copyArray for this.
+ */
 public class MergeSort {
 
     public static int[] sort(int[] array) {
