307ef02a49c12aa0fb43b1e9770809f18e630dd6
beothorn <lucass@gmail.com>
2024-03-09 23:15:40 +0100

    Working parser without function calls

--- javaAgent/src/main/java/com/github/beothorn/agent/parser/ASTNode.java
+++ javaAgent/src/main/java/com/github/beothorn/agent/parser/ASTNode.java
@@ -7,18 +7,18 @@ public class ASTNode {
 
     public Token token;
 
-    public Token[] children;
+    public ASTNode[] children;
 
-    private ASTNode(Token token, Token[] children) {
+    private ASTNode(Token token, ASTNode[] children) {
         this.token = token;
         this.children = children;
     }
 
     public static ASTNode n(Token token) {
-        return new ASTNode(token, new Token[0]);
+        return new ASTNode(token, new ASTNode[0]);
     }
 
-    public static ASTNode n(Token token, Token[] children) {
+    public static ASTNode n(Token token, ASTNode... children) {
         return new ASTNode(token, children);
     }
 
@@ -35,5 +35,13 @@ public class ASTNode {
         int result = Objects.hash(token);
         result = 31 * result + Arrays.hashCode(children);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        if (children.length > 0) {
+            return token.value + Arrays.toString(children);
+        }
+        return token.value;
     }
 }
--- javaAgent/src/test/java/com/github/beothorn/agent/parser/ParserTest.java
+++ javaAgent/src/test/java/com/github/beothorn/agent/parser/ParserTest.java
@@ -2,12 +2,218 @@ package com.github.beothorn.agent.parser;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import static com.github.beothorn.agent.parser.ASTNode.n;
+import static com.github.beothorn.agent.parser.Token.*;
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
 
     @Test
     void simpleParse(){
-        // new Lexer("foo"); mockito here for true unit tests
-//        new Parser()
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+            string("foo")
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(string("foo")),
+            root
+        );
+    }
+    @Test
+    void simpleParseAnd(){
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+                string("foo"),
+                and(),
+                string("bar")
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                and(),
+                n(string("foo")),
+                n(string("bar"))
+            ),
+            root
+        );
+    }
+
+    @Test
+    void simpleParseOr(){
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+            string("foo"),
+            or(),
+            string("bar")
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                or(),
+                n(string("foo")),
+                n(string("bar"))
+            ),
+            root
+        );
+    }
+
+    @Test
+    void simpleParseNot(){
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+            not(),
+            string("foo")
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                not(),
+                n(string("foo"))
+            ),
+            root
+        );
+    }
+
+    @Test
+    void simpleParseManyOperators(){
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+            string("foo"),
+            and(),
+            string("bar"),
+            or(),
+            string("baz")
+        )); // false && true || false = false because left comparision is first = ((false && true) || false)
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                or(),
+                n(
+                    and(),
+                    n(string("foo")),
+                    n(string("bar"))
+                ),
+                n(string("baz"))
+            ),
+            root
+        );
+    }
+
+    @Test
+    void simpleParseParenthesis(){
+        // foo&&(bar||baz)
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+            string("foo"),
+            and(),
+            openParen(),
+            string("bar"),
+            or(),
+            string("baz"),
+            closeParen()
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                and(),
+                n(string("foo")),
+                n(
+                    or(),
+                    n(string("bar")),
+                    n(string("baz"))
+                )
+            ),
+            root
+        );
+    }
+
+    @Test
+    void doubleParenthesis(){
+        // ((foo&&bar)||baz)
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+            openParen(),
+            openParen(),
+            string("foo"),
+            and(),
+            string("bar"),
+            closeParen(),
+            or(),
+            string("baz"),
+            closeParen()
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                or(),
+                n(
+                    and(),
+                    n(string("foo")),
+                    n(string("bar"))
+                ),
+                n(string("baz"))
+            ),
+            root
+        );
+    }
+
+    @Test
+    void singleItemParenthesis(){
+        // (foo&&(bar)||baz)
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+                openParen(),
+                string("foo"),
+                and(),
+                openParen(),
+                string("bar"),
+                closeParen(),
+                or(),
+                string("baz"),
+                closeParen()
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                or(),
+                n(
+                        and(),
+                        n(string("foo")),
+                        n(string("bar"))
+                ),
+                n(string("baz"))
+            ),
+            root
+        );
+    }
+
+    @Test
+    void tripleParenthesis(){
+        //((foo&&(bar)||(baz)))
+        Deque<Token> tokens = new ArrayDeque<>(asList(
+            openParen(),
+            openParen(),
+            string("foo"),
+            and(),
+            openParen(),
+            string("bar"),
+            closeParen(),
+            or(),
+            openParen(),
+            string("baz"),
+            closeParen(),
+            closeParen(),
+            closeParen()
+        ));
+        ASTNode root = Parser.parse(tokens);
+        assertEquals(
+            n(
+                or(),
+                n(
+                    and(),
+                    n(string("foo")),
+                    n(string("bar"))
+                ),
+                n(string("baz"))
+            ),
+            root
+        );
     }
 }
