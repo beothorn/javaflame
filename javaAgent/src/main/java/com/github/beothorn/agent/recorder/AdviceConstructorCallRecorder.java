ebc813b0ceaad955feb6b41d46cfcbebd9804958
beothorn <lucass@gmail.com>
2024-03-30 11:02:31 +0100

    Constructor advice fully working

--- javaAgent/src/main/java/com/github/beothorn/agent/recorder/AdviceConstructorCallRecorder.java
+++ javaAgent/src/main/java/com/github/beothorn/agent/recorder/AdviceConstructorCallRecorder.java
@@ -1,30 +1,40 @@
 package com.github.beothorn.agent.recorder;
 
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.AllArguments;
+import net.bytebuddy.asm.Advice.Origin;
+import net.bytebuddy.asm.Advice.This;
 
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
 import static com.github.beothorn.agent.MethodInstrumentationAgent.LogLevel.DEBUG;
+import static com.github.beothorn.agent.MethodInstrumentationAgent.LogLevel.ERROR;
 import static com.github.beothorn.agent.MethodInstrumentationAgent.log;
 
 public class AdviceConstructorCallRecorder {
 
     @Advice.OnMethodEnter
     public static void enter(
-            @Advice.Origin Constructor<?> constructor,
-            @Advice.AllArguments Object[] allArguments
+            @Origin Constructor<?> constructor,
+            @AllArguments Object[] allArguments
     ) {
-        System.out.println("CONSTRUCTOR!!!!!");
         try {
             FunctionCallRecorderWithValueCapturing.enterConstructor(constructor, allArguments);
         } catch (Exception e){
-            e.printStackTrace();
-            log(DEBUG, e.getMessage());
+            log(ERROR, "On enter constructor "+e.getMessage());
+            log(DEBUG, Arrays.toString(e.getStackTrace()));
         }
     }
-//    @Advice.OnMethodExit(onThrowable = Throwable.class)
-//    public static void exit(
-//    ) {
-//        FunctionCallRecorderWithValueCapturing.exit(null);
-//    }
+    @Advice.OnMethodExit
+    public static void exit(
+            @This Object self
+    ) {
+        try{
+            FunctionCallRecorderWithValueCapturing.exit(self);
+        } catch (Exception e){
+            log(ERROR, "On exit constructor "+e.getMessage());
+            log(DEBUG, Arrays.toString(e.getStackTrace()));
+        }
+    }
 }
--- javaAgent/src/main/java/com/github/beothorn/agent/recorder/AdviceFunctionCallRecorder.java
+++ javaAgent/src/main/java/com/github/beothorn/agent/recorder/AdviceFunctionCallRecorder.java
@@ -1,30 +1,41 @@
 package com.github.beothorn.agent.recorder;
 
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.AllArguments;
+import net.bytebuddy.asm.Advice.OnMethodExit;
+import net.bytebuddy.asm.Advice.Origin;
+import net.bytebuddy.asm.Advice.Return;
 import net.bytebuddy.implementation.bytecode.assign.Assigner;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static com.github.beothorn.agent.MethodInstrumentationAgent.LogLevel.DEBUG;
+import static com.github.beothorn.agent.MethodInstrumentationAgent.LogLevel.ERROR;
 import static com.github.beothorn.agent.MethodInstrumentationAgent.log;
 
 public class AdviceFunctionCallRecorder {
     @Advice.OnMethodEnter
     public static void enter(
-            @Advice.Origin Method method,
-            @Advice.AllArguments Object[] allArguments
+            @Origin Method method,
+            @AllArguments Object[] allArguments
     ) {
         try {
             FunctionCallRecorderWithValueCapturing.enterFunction(method, allArguments);
         } catch (Exception e){
-            e.printStackTrace();
-            log(DEBUG, e.getMessage());
+            log(ERROR, "On enter function "+e.getMessage());
+            log(DEBUG, Arrays.toString(e.getStackTrace()));
         }
     }
-    @Advice.OnMethodExit(onThrowable = Throwable.class)
+    @OnMethodExit(onThrowable = Throwable.class)
     public static void exit(
-            @Advice.Return(typing = Assigner.Typing.DYNAMIC) Object returnValueFromMethod
+            @Return(typing = Assigner.Typing.DYNAMIC) Object returnValueFromMethod
     ) {
-        FunctionCallRecorderWithValueCapturing.exit(returnValueFromMethod);
+        try {
+            FunctionCallRecorderWithValueCapturing.exit(returnValueFromMethod);
+        } catch (Exception e) {
+            log(ERROR, "On exit function " + e.getMessage());
+            log(DEBUG, Arrays.toString(e.getStackTrace()));
+        }
     }
 }
